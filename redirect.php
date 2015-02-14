b8eda501ad4883bb94273229b0c63b6a0e78c6ed
maytop <maytop@users.noreply.github.com>
2015-02-14 21:15:33 +0100

    Fix HTTPS links are linked wrong #7

--- redirect.php
+++ redirect.php
@@ -22,8 +22,9 @@ $url = str_replace('&gt;', '>', $url);
 $url = str_replace('\\', '', $url);
 
 if ($url) {
-    if (substr($url, 0, 7) != "http://")
+    if (substr($url, 0, 7) != "http://" && substr($url, 0, 8) != "https://") {
         $url = "http://" . $url;
+    }
 ?>
 <html><head>
 <title>DEREFER</TITLE><META CHARSET=UTF-8>
